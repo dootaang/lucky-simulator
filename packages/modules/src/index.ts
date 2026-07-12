8f58b7baab4d681c000b39d4473add8181f97fd5
dootaang <kksk980@gmail.com>
2026-07-12 10:26:24 +0900

    module: migrate deterministic combat runtime

--- packages/modules/src/index.ts
+++ packages/modules/src/index.ts
@@ -2,5 +2,6 @@ export * from './support.ts';
 export * from './common.ts';
 export * from './advanced.ts';
 export * from './hunter.ts';
-import { ModuleRegistry } from '@simbot/kernel'; import { commonModules } from './common.ts'; import { craftingModule,equipmentModule,questsModule,shopModule } from './advanced.ts'; import { hunterModule } from './hunter.ts';
-export function createStandardRegistry(){const registry=new ModuleRegistry();for(const module of commonModules())registry.register(module);for(const module of [equipmentModule(),questsModule(),shopModule(),craftingModule(),hunterModule()])registry.register(module);return registry;}
+export * from './combat.ts';
+import { ModuleRegistry } from '@simbot/kernel'; import { commonModules } from './common.ts'; import { craftingModule,equipmentModule,questsModule,shopModule } from './advanced.ts'; import { hunterModule } from './hunter.ts'; import { combatModule,statsModule } from './combat.ts';
+export function createStandardRegistry(){const registry=new ModuleRegistry();registry.register(statsModule());for(const module of commonModules())registry.register(module);for(const module of [equipmentModule(),questsModule(),shopModule(),craftingModule(),hunterModule(),combatModule()])registry.register(module);return registry;}
